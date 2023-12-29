daf87a89c91d311ce208b6f8c6748e12be99a345
GustavoBehnckCardoso <gustavo.cardoso@flipfloplab.com.br>
2023-12-29 13:03:52 -0300

    Add Adress and more

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < ApplicationRecord
     belongs_to :kind
     has_many :phones
+    accepts_nested_attributes_for :phones, allow_destroy: true
 
     def as_json(options={})
-        super(
+        h = super(
                 root: true,
                 except: [:created_at, :updated_at, :kind_id],
                 include: {
@@ -11,10 +12,12 @@ class Contact < ApplicationRecord
                         except: [:created_at, :updated_at]
                     },
                     phones:{
-                        except: [:created_at, :updated_at, :id, :contact_id]
+                        except: [:created_at, :updated_at, :contact_id]
                     }
                 }
-        )
+            )
+        h["contact"]["birthdate"] = ((self.birthdate).strftime('%d-%m-%Y') unless self.birthdate.blank?)
+        h
     end
 
 end
