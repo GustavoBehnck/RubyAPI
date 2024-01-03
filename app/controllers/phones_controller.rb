56d81a589c38b7affbf85e5af182ec95fbd726de
GustavoBehnckCardoso <gustavo.cardoso@flipfloplab.com.br>
2024-01-03 10:24:33 -0300

    More about phones

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -5,10 +5,37 @@ class PhonesController < ApplicationController
         render json: @contact.phones
     end
 
+    def destroy
+        phone = Phone.find(phone_params[:id])
+        phone.destroy
+    end
+    
+    def update
+        phone = Phone.find(phone_params[:id])
+        if phone.update(phone_params)
+            render json: @contact.phones
+        else
+            render json @contact.errors
+        end
+    end
+
+    def create
+        @contact.phones << Phone.new(phone_params)
+        if @contact.save 
+            render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+        else
+            render json @contact.errors
+        end
+    end
+
 
     private
     # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-        @contact = params[:contact_id] ? Contact.find(params[:contact_id]) : { 'error': 'You didnt inform an id'}
-    end
+        def set_contact
+            @contact = params[:contact_id] ? Contact.find(params[:contact_id]) : { 'error': 'You didnt inform an id'}
+        end
+
+        def phone_params
+            ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+        end
 end
